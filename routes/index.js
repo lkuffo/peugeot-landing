5d0cec1fc6944801db1d6a83c7727ea6ff8b2732
Leonardo Xavier Kuffo Rivero <lkuffo@espol.edu.ec>
2019-02-14 07:25:52 -0500

    [Eurotrip] Landing page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/301-diesel', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('301-diesel', { title: 'Express' });
+});
+
+router.get('/eurotrip', function(req, res, next) {
+  res.render('eurotrip', { title: 'Express' });
 });
 
 module.exports = router;
