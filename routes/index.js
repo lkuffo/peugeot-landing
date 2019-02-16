6ea2cd7102453c8178dcb4a7f7b74250e4fcc82a
Leonardo Xavier Kuffo Rivero <lkuffo@espol.edu.ec>
2019-02-16 17:43:30 -0500

    [Refactor] Refactoring and thanks page

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/301-diesel', function(req, res, next) {
-  res.render('301-diesel', { title: 'Express' });
+  res.render('301-diesel/301-diesel', { title: 'Express' });
+});
+
+router.get('/301-diesel-thanks', function(req, res, next) {
+  res.render('301-diesel/301-diesel-thanks', { title: 'Express' });
 });
 
 router.get('/eurotrip', function(req, res, next) {
-  res.render('eurotrip', { title: 'Express' });
+  res.render('eurotrip/eurotrip', { title: 'Express' });
+});
+
+router.get('/eurotrip-thanks', function(req, res, next) {
+  res.render('eurotrip/eurotrip-thanks', { title: 'Express' });
 });
 
 module.exports = router;
